842aa144347c1d493e4128b7e3997cdbc6454332
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-03 11:28:13 +0100

    Ejercicio 4 copiado

---
+++ 4.Escalera.py
@@ -0,0 +1,12 @@
+import math 
+import os 
+import random 
+import re 
+import sys
+
+def staircase(n):
+    
+
+if __name__ == '__main__':     
+    n = int(input().strip())      
+    staircase(n) 
